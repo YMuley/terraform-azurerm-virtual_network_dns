98785392ae902362e709b6998d17904cd89c289d
Yogesh Muley <Yogesh.Muley@MOTIFWORKS.IN>
2023-10-25 20:28:05 +0530

    locals.tf file updated

--- locals.tf
+++ locals.tf
@@ -1,3 +1,3 @@
 locals {
-    virtual_network_dns = {for name in var.virtual_network_dns_list : name.virtual_network_name => virtual_network_name }
+    virtual_network_dns = {for name in var.virtual_network_dns_list : name.virtual_network_name => name }
 }
