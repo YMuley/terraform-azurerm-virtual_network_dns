65bb6972abb6f104b299083cbeaacac8760c5d50
Yogesh Muley <Yogesh.Muley@MOTIFWORKS.IN>
2023-10-26 13:06:32 +0530

    code updated

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_network_dns_servers" "virtual_network_dns" {
   for_each = local.virtual_network_dns
   virtual_network_id = var.virtual_network_output[each.value.virtual_network_name].id
-  dns_servers = length(each.value.dns_servers) == 0 ? ["Default"] : each.value.dns_servers
+  dns_servers = length(each.value.dns_servers) == 0 ? null : each.value.dns_servers
 }
